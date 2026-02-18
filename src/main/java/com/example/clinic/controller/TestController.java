072e3798648a4bea6898270f2c6ee4ff428ad7de
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-02-18 19:18:22 +0300

    feat: enable role-based authorization using @PreAuthorize

--- src/main/java/com/example/clinic/controller/TestController.java
+++ src/main/java/com/example/clinic/controller/TestController.java
@@ -1,15 +1,29 @@
 package com.example.clinic.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/test")
 public class TestController {
 
-    @GetMapping
-    public String securedEndpoint(){
-        return "You are authenticated";
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminEndpoint() {
+        return "Hello Admin!";
+    }
+
+    @GetMapping("/doctor")
+    @PreAuthorize("hasRole('DOCTOR')")
+    public String doctorEndpoint() {
+        return "Hello Doctor!";
+    }
+
+    @GetMapping("/reception")
+    @PreAuthorize("hasRole('RECEPTIONIST')")
+    public String receptionEndpoint() {
+        return "Hello Receptionist!";
     }
 }
